30794542767dee3c635d1b7162d2b1e1471ee6a6
jon prichard <jon.prichard@phila.gov>
2024-12-03 15:42:18 -0500

    Updating to 1.7.4
    - Per austin, 1.7.4 adds SQL Server Compatibility

---  _variables.tf
+++  _variables.tf
@@ -32,7 +32,7 @@ variable "owner" {
 variable "phdi_version" {
   description = "PHDI container image version"
   type        = string
-  default     = "v1.7.3"
+  default     = "v1.7.4"
 }
 
 variable "private_subnets" {
