29eee33be1b2ad9f821b07f5f7c3fec13984e131
Chris Stevens <christopher.stevens@phila.gov>
2026-07-23 17:34:04 -0400

    Port PDPH environment config onto upstream-rebuild base

--- terraform/implementation/ecs/main.tf
+++ terraform/implementation/ecs/main.tf
@@ -1,121 +1,92 @@
 data "aws_acm_certificate" "this" {
-  domain   = "*.dibbs.cloud"
-  types    = ["AMAZON_ISSUED"] # or ["ISSUED"] or ["PRIVATE"]
+  domain   = var.cert_name
+  types    = ["AMAZON_ISSUED"]
   statuses = ["ISSUED"]
-}
-
-module "vpc" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=9ffd9c66f3d7eb4b5bc2d7bc7d049f794b127693" # 5.16.0
-
-  name            = local.vpc_name
-  cidr            = var.vpc_cidr
-  azs             = var.availability_zones
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-  # if internal is true, then the VPC will not have a NAT or internet gateway
-  enable_nat_gateway = var.internal ? false : true
-  single_nat_gateway = var.internal ? false : true
-  create_igw         = var.internal ? false : true
-  tags               = local.tags
-}
-
-module "db" {
-  source = "../../modules/db"
-
-  vpc_id             = module.vpc.vpc_id
-  cidr               = var.vpc_cidr
-  owner              = var.owner
-  project            = var.project
-  tags               = local.tags
-  private_subnet_ids = flatten(module.vpc.private_subnets)
-  public_subnet_ids  = flatten(module.vpc.public_subnets)
-  ssh_key_name       = var.ssh_key_name
-  # determines which database is launched, required for the ec2 instance to know which database to setup
-  database_type = var.database_type
 }
 
 module "ecs" {
   source = "git::https://github.com/CDCgov/terraform-aws-dibbs-ecr-viewer.git?ref=292d3057adf75303689deeedd89b46c3462faf27"
 
-  public_subnet_ids  = flatten(module.vpc.public_subnets)
-  private_subnet_ids = flatten(module.vpc.private_subnets)
-  vpc_id             = module.vpc.vpc_id
-  region             = var.region
+  public_subnet_ids  = flatten(var.public_subnets)
+  private_subnet_ids = flatten(var.private_subnets)
 
-  owner        = var.owner
-  project      = var.project
-  tags         = local.tags
-  phdi_version = var.phdi_version
+  vpc_id = var.vpc_id
+  region = var.region
 
-  # The following variables will need to be configured depending on your requirements
-  # If intent is to pull from the dibbs-ecr-viewer GHCR, set disable_ecr to true (default is false when not set)
-  disable_ecr = false
+  owner   = var.owner
+  project = var.project
+  tags    = local.tags
 
-  enable_enhanced_ecr_registry_scanning = true
+  # So allow URI path rule doesn't get tossed out:
+  waf_web_acl_arn = var.waf_web_acl_arn
 
-  # If the intent is to make the ecr-viewer available on the public internet, set internal to false (default is true when not set)
-  # This requires an internet gateway to be present in the VPC.
-  internal = var.internal
-
-  # If the intent is to enable https and port 443, pass the arn of the cert in AWS certificate manager. This cert will be applied to the load balancer. (default is "" when not set)
+  # Pass cert arn to module
   certificate_arn = data.aws_acm_certificate.this.arn
 
-  # To disable autoscaling, set enable_autoscaling to false (default is true when not set)
-  enable_autoscaling = true
+  db_cipher         = var.db_cipher
+  dibbs_config_name = var.ecr_viewer_database_schema
 
-  enable_alb_deletion_protection = false
+  disable_ecr = false
 
-  cw_retention_in_days = 14
+  internal = true
 
-  ecr_viewer_object_retention_days = 14
+  phdi_version = var.phdi_version
 
-  logging_object_retention_days = 14
+  auth_provider              = var.auth_provider
+  auth_client_id             = var.auth_client_id
+  auth_issuer                = var.auth_issuer
+  auth_url                   = var.auth_url
+  auth_session_duration_min  = var.auth_session_duration_min
 
-  # If the intent is to enable alb deletion protection, set false (default is true when not set)
-  # enable_alb_deletion_protection = false
+  # GitHub Actions secrets, injected as plaintext ECS environment variables at apply time
+  secrets_manager_connection_string_version                   = var.sql_connection_string
+  secrets_manager_auth_secret_version                         = var.nextauth_secret
+  secrets_manager_auth_client_secret_version                  = var.auth_client_secret
+  secrets_manager_metadata_database_migration_secret_version  = var.secrets_manager_metadata_database_migration_secret_version
 
-  # If intent is to use a metadata database for the ecr-viewer library, provider the required secrets manager names
-  # Postgresql database example (default is "" when not set)
-  secrets_manager_connection_string_version = var.database_type == "postgresql" || var.database_type == "sqlserver" ? module.db.secrets_manager_database_connection_string_version : ""
+  # ecr_viewer_auth_pub_key / ecr_viewer_auth_api_pub_key intentionally
+  # NOT set here. See the warning on var.nbs_pub_key in _variable.tf --
+  # PDPH does not currently use pub-key auth validation, and wiring this
+  # in would silently enable it against upstream's example key.
 
-  # SqlServer database example (default values are "" when not set)
-  # secrets_manager_sqlserver_user_version = module.db.secrets_manager_sqlserver_user_version
-  # secrets_manager_sqlserver_password_version = module.db.secrets_manager_sqlserver_password_version
-  # secrets_manager_sqlserver_host_version = module.db.secrets_manager_sqlserver_host_version
-
-  # dibbs_config_name can be a value found here under CONFIG_NAME: https://github.com/CDCgov/dibbs-ecr-viewer/blob/main/containers/ecr-viewer/environment.d.ts
-  # This is used to configure the ecr-viewer application. (default is "" when not set)
-  dibbs_config_name = var.dibbs_config_name
-
-  # non integrated auth provider example (default values are "" when not set)
-  auth_provider                                              = var.auth_provider
-  auth_client_id                                             = var.auth_client_id
-  auth_issuer                                                = var.auth_issuer
-  auth_url                                                   = var.auth_url
-  auth_session_duration_min                                  = var.auth_session_duration_min
-  secrets_manager_auth_secret_version                        = var.secrets_manager_auth_secret_version
-  secrets_manager_auth_client_secret_version                 = var.secrets_manager_auth_client_secret_version
-  secrets_manager_metadata_database_migration_secret_version = var.secrets_manager_metadata_database_migration_secret_version
-
-  ecr_viewer_auth_pub_key     = var.nbs_pub_key
-  ecr_viewer_auth_api_pub_key = var.nbs_pub_key
-
+  # Set Container Size
   override_autoscaling = {
-    fhir-converter = {
-      cpu           = 2048
-      memory        = 4096
-      max_capacity  = 5
-      min_capacity  = 1
-      target_cpu    = 60
-      target_memory = 70
-    }
+    ecr-viewer             = var.task_size_overrides[0]
+    fhir-converter         = var.task_size_overrides[1]
+    ingestion              = var.task_size_overrides[2]
+    message-parser         = var.task_size_overrides[3]
+    orchestration          = var.task_size_overrides[4]
+    trigger-code-reference = var.task_size_overrides[5]
   }
+
+  # Logging for ALB
+  enable_alb_logs        = var.enable_alb_logs
+  s3_logging_bucket_name = "${var.project}-${var.owner}-${terraform.workspace}-logging-bucket"
+
+  # Timeouts
+  alb_idle_timeout       = 3600    # seconds
+  ecr_processing_timeout = 3600000 # miliseconds
+
+  # Explicitly set to match current effective values
+  cw_retention_in_days                  = 365
+  ecr_viewer_object_retention_days      = 365
+  logging_object_retention_days         = 365
+  enable_alb_deletion_protection        = true
+  enable_enhanced_ecr_registry_scanning = false
+}
+
+data "aws_lb" "ecs" {
+  arn = module.ecs.alb_arn
 }
 
 resource "aws_route53_record" "alb" {
   zone_id = var.route53_hosted_zone_id
-  name    = "${terraform.workspace}.dibbs.cloud"
-  type    = "CNAME"
-  ttl     = 60
-  records = [module.ecs.alb_dns_name]
+  name    = var.cert_name
+  type    = "A"
+
+  alias {
+    name                   = module.ecs.alb_dns_name
+    zone_id                = data.aws_lb.ecs.zone_id
+    evaluate_target_health = true
+  }
 }
