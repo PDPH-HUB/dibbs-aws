c8ec4168542c0c89a96edb57de2b7a260a7b605e
Chris Stevens <christopher.stevens@phila.gov>
2026-07-30 22:52:57 -0400

    Drop workspace segment from oidc role name to match live roles (#4)

--- terraform/modules/oidc/_local.tf
+++ terraform/modules/oidc/_local.tf
@@ -1,5 +1,5 @@
 locals {
-  github_role_name = "${var.project}-github-role-${var.owner}-${var.workspace}-${random_string.oidc.result}"
+  github_role_name = "${var.project}-github-role-${var.owner}-${random_string.oidc.result}"
   wildcard         = "*"
   vpc_id           = var.vpc_id == "" ? local.wildcard : var.vpc_id
 }
